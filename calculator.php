05cd6c5620d25f23437d3c70257e571ca8f0c02b
Itx-junaid00 <137100733+Itx-junaid00@users.noreply.github.com>
2023-11-06 21:56:56 +0500

    calculator

---
+++ calculator.php
@@ -0,0 +1,25 @@
+<?php
+if(isset($_POST['calculate']))
+{
+    $num1 = $_POST['num1'];
+    $num2 = $_POST['num2'];
+    $operator = $_POST['operator'];
+
+switch($operator)
+{
+case 'add':
+    $result = $num1 + $num2 ;
+    break;
+case 'sub':
+    $result = $num1 - $num2 ;
+    break;
+case 'mul':
+    $result = $num1 * $num2 ;
+    break;
+case 'div':
+    $result = $num1 / $num2 ;
+    break;
+}
+echo $result;
+}
+?>
